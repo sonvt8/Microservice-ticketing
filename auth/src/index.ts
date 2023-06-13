226510ffcabe574a00e49e29b988507555342a1c
sonvt8 <vuthaison198x@gmail.com>
2023-06-13 17:11:27 +0700

    creating Route Handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './route/current-user';
+import { signinRouter } from './route/signin';
+import { signoutRouter } from './route/signout';
+import { signupRouter } from './route/signup';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req,res) => {
-  res.send('Hi there');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, ()=> {
   console.log('listening on port 3000');
