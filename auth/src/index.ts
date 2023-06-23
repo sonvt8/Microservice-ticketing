cb55c63daa113c7a6867d6bf8e71647f9ce27772
sonvt8 <vuthaison198x@gmail.com>
2023-06-23 20:31:27 +0700

    Writing some test cases

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './route/current-user';
-import { signinRouter } from './route/signin';
-import { signoutRouter } from './route/signout';
-import { signupRouter } from './route/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { app } from './app';
 import { DatabaseConnectionError } from './errors/database-connection-error';
 
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-)
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res)=> {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 const start = async () => {
   if(!process.env.JWT_KEY){
